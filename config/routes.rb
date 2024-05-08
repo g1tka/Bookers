1be53a1c7365fcea234f7c90dd58be97de66f6ac
g1tka <165782083+g1tka@users.noreply.github.com>
2024-05-08 12:37:15 +0000

    []3-16

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 #  get 'books/edit'
 #  get 'homes/top'
   resources :books
+  get "books/:id" => 'books#show', as: 'book'
+  get "books/:id/edit" => 'books#edit', as: 'edit_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
