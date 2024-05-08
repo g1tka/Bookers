e14ac3aee0313eba625b3b88887db26f49233a5d
g1tka <165782083+g1tka@users.noreply.github.com>
2024-05-08 14:29:19 +0000

    [Finish]3-16

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,15 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  #       POST   /books(.:format)              books#create
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
+  end  
+    
   def index
     @books = Book.all
   end
@@ -8,4 +19,16 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 #  get 'books/show'
 #  get 'books/edit'
 #  get 'homes/top'
-  resources :books
-  get "books/:id" => 'books#show', as: 'book'
+  resources :books, except: [:show, :edit, :destroy]
+  get "books/:id" => 'books#show', as: 'show_book'
   get "books/:id/edit" => 'books#edit', as: 'edit_book'
+  delete 'books/:id' => 'lists#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
